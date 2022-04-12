887318ae54fab455c0781ae25b56ef23e02f3969
SorokaBelaboka <92310332+SorokaBelaboka@users.noreply.github.com>
2022-04-12 19:18:45 +0500

    Добавлена тень и пушка

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,7 +9,7 @@ export (float, 0, 1) var acceleration
 var _velocity := Vector2()
 
 onready var _body_pos: Vector2 = $body_sprite.position
-onready var _gun_pos: Vector2 = $gun_sprite.position
+
 
 func _ready() -> void:
 	$body_sprite.playing = true
@@ -21,15 +21,16 @@ func _process(delta: float) -> void:
 	if mouse != null:
 		if mouse.x > position.x:
 			$body_sprite.flip_h = false
+			$gun_sprite.flip_h = false
 		elif mouse.x < position.x:
 			$body_sprite.flip_h = true
+			$gun_sprite.flip_h = true
 
 
 func _physics_process(delta: float) -> void:
 	# Плавно движемся по направлению ввода
 	_velocity = _velocity.linear_interpolate(InputHandler.input_direction * move_speed, acceleration)
-	_velocity = move_and_slide(_velocity)
-	$body_sprite.position = _body_pos - position + position.round()
+	_velocity = move_and_slide(_velocity)	
 	# Заявляем функцию по изменению анимаций персонажа
 	_player_animation()
 	
@@ -43,16 +44,27 @@ func _player_animation() -> void:
 		$body_sprite.animation = "run_back"
 	else:
 		$body_sprite.animation = "run"
+		
+	#if Input.action_press("shoot"):
+		
 	
 	
 
 
-func _on_gun_sprite_frame_changed():
-	if $body_sprite.animation == 'run':
+func _on_body_sprite_frame_changed():
+	if $body_sprite.animation == "run":
 		if $body_sprite.frame == 4 or $body_sprite.frame == 8:
-			_gun_pos.y += 1
-		else: _gun_pos = _body_pos
-	elif $body_sprite.animation == 'run_back':
+			$gun_sprite.position.y += 1 
+		else:
+			$gun_sprite.position = _body_pos
+	elif $body_sprite.animation == "run_back":
 		if $body_sprite.frame == 4 or $body_sprite.frame == 0:
-			_gun_pos.y += 1
-		else: _gun_pos = _body_pos
+			$gun_sprite.position.y += 1 
+		else:
+			$gun_sprite.position = _body_pos
+	elif $body_sprite.animation == "idle":
+		if $body_sprite.frame == 2:
+			$gun_sprite.position.y += 1
+		else:
+			$gun_sprite.position = _body_pos
+			
