f47bdd4348c6a2c9bb366fbba457da3b3d5ca2a2
SorokaBelaboka <92310332+SorokaBelaboka@users.noreply.github.com>
2022-04-18 01:35:36 +0500

    машина состояний
    все работает, но поломан дэш(не врубать!!!)

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,10 +1,4 @@
 extends Camera2D
 
-onready var player = get_parent()
-
-var window_size := OS.get_window_size()
-var distance_to_mouse_pos: float = 0
-	
-	
 func _physics_process(delta: float) -> void:
 	position = get_local_mouse_position() / Vector2(3, 2)
--- scripts/player.gd
+++ scripts/player.gd
@@ -5,78 +5,112 @@ export (float, 0, 1) var acceleration # Вес интерполяции скор
 export var dash_enable: bool = false # Режим рывков (вкл/выкл)
 export var dash_speed: int = 1000 # Скорость рывка
 export var dash_length: float = 0.2 # Длинна рывка(влияет на таймер рывка и его длину)
-export var dash_reload_time: float = 3 # Скорость перезарядки N рывков
+export var dash_reload_time: float = 2 # Скорость перезарядки N рывков
 export var dash_number: int = 5 # Кол-во возможных зарядов для рывков
 export(PackedScene) var dash_effect_scene
 
 var _velocity := Vector2() # Скорость персонажа на данный момент (px/s)
-var state := str("") # Состояние персонажа  
 var mouse := Vector2()# Глобальная позиция мыши
-var dash_counter := int() # Кол-во доступных зарядов для рывков
+var dash_counter = 0 # Кол-во доступных зарядов для рывков
 var dash_direction := Vector2() # направление рывка
+var state
 
+enum states {
+	idle,
+	run,
+	dash,
+	is_dashing,
+	dash_ended
+}
 
 func _ready() -> void:
 	$body_sprite.playing = true
 	$body_sprite/body_flash_sprite.playing = true
-	
-	InputHandler.connect("mouse_pressed", self, "_on_mouse_pressed")
-	InputHandler.connect("dash", self, "_dash")
-	
+#	InputHandler.connect("mouse_pressed", self, "_on_mouse_pressed")
+#	InputHandler.connect("dash", self, "_dash")
 	dash_counter = dash_number
+	state = states.idle
 
 
 func _process(delta: float) -> void:
 	$body_sprite/body_flash_sprite.animation = $body_sprite.animation
-	
 	$body_sprite/body_flash_sprite.flip_h = $body_sprite.flip_h
-	var mouse := get_global_mouse_position()
-	# Разворачиваем спрайт игрока по направлению к мыши
-	if mouse != null:
-		if mouse.x > position.x:
-			$body_sprite.flip_h = false
-#			$gun_sprite.flip_h = false
-		elif mouse.x < position.x:
-			$body_sprite.flip_h = true
-#			$gun_sprite.flip_h = true
-	$label.set_text(state)
-
+	mouse = get_global_mouse_position()
+	_turn_to_mouse() # Разворачиваем спрайт игрока по направлению к мыши
+	if state == states.idle:
+		$label.set_text("idle")
+	elif state == states.dash:
+		$label.set_text("dash")
+	else:
+		$label.set_text("run")
 
 func _physics_process(delta: float) -> void:
-	mouse = get_global_mouse_position()
-	if state == "dash":
-		_dash()
-		_velocity = dash_direction
-		_velocity = move_and_slide(_velocity)
-	else:
-		_velocity = _velocity.linear_interpolate(InputHandler.input_direction * move_speed, acceleration) # Задаем вектор и скорость перемещения с учетом ускорения
-		_velocity = move_and_slide(_velocity) # Плавно движемся по направлению ввода
+	_state_machine()
+	_move_player() # Перемещения игрока
 	_player_animation() # Заявляем функцию по изменению анимаций персонажа
+	_dash()
 	
 
 
 func _player_animation() -> void:
-	if _velocity.length() < 100 and state != "dash":
+	if state == states.idle:
 		$body_sprite.animation = "idle"
-		state = "idle"
-	elif (mouse.x > position.x and _velocity.x < 0) or (mouse.x < position.x and _velocity.x > 0) and state != "dash": #Сверяем движется ли персонаж и как именно он движется(спиной/лицом) 
-		$body_sprite.animation = "run_back"
-		state = "run"
-	elif state != "dash":
-		$body_sprite.animation = "run"
-		state = "run"
+	else:
+		if ((mouse.x > position.x and _velocity.x < 0) or (mouse.x < position.x and _velocity.x > 0)): #Сверяем движется ли персонаж и как именно он движется(спиной/лицом) 
+			$body_sprite.animation = "run_back"
+		else:
+			$body_sprite.animation = "run"
 
+
+func _turn_to_mouse() -> void:
+	if mouse.x > position.x: # Разворачиваем спрайт игрока по направлению к мыши
+		$body_sprite.flip_h = false
+	elif mouse.x < position.x:
+		$body_sprite.flip_h = true
+
+
+func _move_player() -> void:
+	if state == states.dash:
+		_create_dash_effect() # Создаем эффект от рывка
+		_velocity = dash_direction  # Задаем вектор и скорость рывка
+		move_and_slide(_velocity)# Производим рывок
+	elif state == states.run:
+		_velocity = _velocity.linear_interpolate(InputHandler.input_direction * move_speed, acceleration) # Задаем вектор и скорость перемещения с учетом ускорения
+		_velocity = move_and_slide(_velocity) # Плавно движемся по направлению ввода
+	elif state == states.idle:
+		_velocity = _velocity.linear_interpolate(InputHandler.input_direction * move_speed, acceleration) # Задаем вектор и скорость перемещения с учетом ускорения
+		_velocity = move_and_slide(_velocity) # Плавно движемся по направлению ввода
+
+
+func _state_machine() -> void:
+	if state == states.idle:
+		if InputHandler.input_direction != Vector2.ZERO:
+			state = states.run
+	elif state == states.run:
+		if Input.is_action_just_pressed("dash"):
+			state = states.dash
+		elif InputHandler.input_direction == Vector2.ZERO:
+			state = states.idle
+	elif state == states.dash_ended:
+		if InputHandler.input_direction != Vector2.ZERO:
+			state = states.run
+		else:
+			state = states.idle
+
+#____________________________________________________________________________________________
 
 func _dash() -> void:
 	if dash_enable:
-		if state == "run" and dash_counter != 0:
-			state = "dash"
+		if state == states.dash and dash_counter != 0:
+			state = states.is_dashing
+			dash_counter -= 1
 			dash_direction = InputHandler.input_direction * dash_speed
 			$dash_timer.start(dash_length)
-		elif state != "dash" and dash_counter == 0:
-			$dash_reload.start(dash_reload_time)
-	
-	if state == "dash":
+			print(dash_direction)
+
+
+func _create_dash_effect() -> void:
+	if state == states.is_dashing:
 		var dash_effect_node = dash_effect_scene.instance()
 		dash_effect_node.texture = $body_sprite.frames.get_frame($body_sprite.animation, $body_sprite.frame)
 		dash_effect_node.position = position + Vector2(1, -22)
@@ -85,12 +119,13 @@ func _dash() -> void:
 
 
 func _on_dash_timer_timeout() -> void:
-	state = " "
-	dash_number -= 1
+	state = states.dash_ended
 	_player_animation()
-	pass
+	if state != states.dash and dash_counter == 0:
+		$dash_reload.start(dash_reload_time)
 
 
 func _on_dash_reload_timeout() -> void:
-	dash_counter = 5
-	pass
+	dash_counter = dash_number
+
+#____________________________________________________________________________________________
