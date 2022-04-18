9ee7cbb47820da9b3ea068b151c021221bb6ade8
ShpakovDaniel <44341164+Reltig@users.noreply.github.com>
2022-04-18 21:37:35 +0500

    стэйт машина для пушки

--- scripts/gun_mk_bit.gd
+++ scripts/gun_mk_bit.gd
@@ -2,13 +2,24 @@ extends AnimatedSprite
 
 export(PackedScene) var bullet_scene
 
+var state
+var bullet_number:int = 30
+
+enum states{
+	idle,
+	shoot,
+	reload
+}
+
 
 func _ready() -> void:
 	InputHandler.connect("left_button_clicked", self, "_shoot_bullet")
+	state = states.idle
 
 
 func _process(delta) -> void:
 	_rotation()
+	_state_machine()
 	$flash_sprite.flip_v = flip_v
 	$flash_sprite.animation = animation
 	$flash_sprite.offset = offset
@@ -31,4 +42,16 @@ func _rotation() -> void:
 func _shoot_bullet() -> void:
 	var bullet = bullet_scene.instant()
 	bullet.rotation = rotation
-	bullet.direction = $direction/direction_shape
+	bullet.direction = $end.position - $begin.position
+	bullet.global_position = $end.global_position
+	get_tree().get_root().add_child(bullet)
+	
+
+func _state_machine():
+	if state == states.idle:
+		if Input.is_action_just_pressed("shoot"):
+			state = states.shoot
+		elif Input.is_action_just_pressed("reload"):
+			state = states.reload
+	elif state == states.shoot:
+		pass
