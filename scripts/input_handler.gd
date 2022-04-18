62959af98f9f48d622597ff6f595de611cb49311
SorokaBelaboka <92310332+SorokaBelaboka@users.noreply.github.com>
2022-04-18 18:09:42 +0500

    закрытие игры на esc

--- scripts/input_handler.gd
+++ scripts/input_handler.gd
@@ -6,6 +6,7 @@ var input_direction := Vector2()
 
 signal mouse_pressed 
 signal dash
+signal exit
 
 func _physics_process(delta: float) -> void:
 	input_direction = Vector2(
@@ -17,4 +18,6 @@ func _input(event: InputEvent) -> void:
 		emit_signal("mouse_pressed")
 	if event.is_action_pressed("dash"):
 		emit_signal("dash")
+	if event.is_action_pressed("esc"):
+		emit_signal("exit")
 
