97b43ca29191982173343ec7d28b5fa9e700de9e
SorokaBelaboka <92310332+SorokaBelaboka@users.noreply.github.com>
2022-04-21 22:30:33 +0500

    доделано за Ильей катушка теслы

--- scripts/thunder_bolt.gd
+++ scripts/thunder_bolt.gd
@@ -12,8 +12,9 @@ func _ready():
 func _physics_process(delta):
 	if is_colliding():
 		var coll_point = to_local(get_collision_point())
-		line.points[1].x  = coll_point.x
+		line.points[1].y  = coll_point.y
 		end.position = coll_point
 	else:
 		cast_to = Vector2(0,max_distance)
 		end.position = Vector2(0,max_distance)
+		line.points[1].y  = max_distance
