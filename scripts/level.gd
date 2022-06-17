d29bb24ab6aee42bd31cc1fa0d74a1d42529a8e4
altered <altered_games@outlook.com>
2022-06-17 17:11:47 +0500

    ура кодекс

--- scripts/level.gd
+++ scripts/level.gd
@@ -2,3 +2,9 @@ extends Node2D
 
 func _ready():
 	$world_environment.environment.glow_enabled = true
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("open_codex"):
+		get_tree().paused = !get_tree().paused
+		$canvas_layer/codex.visible = !$canvas_layer/codex.visible
