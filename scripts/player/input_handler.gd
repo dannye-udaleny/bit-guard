af5ffaebf577a318411a9ee5b1288f852bf5e81e
ShpakovDaniel <44341164+Reltig@users.noreply.github.com>
2022-06-17 20:51:14 +0500

    перезарядка на R

--- scripts/player/input_handler.gd
+++ scripts/player/input_handler.gd
@@ -9,6 +9,7 @@ signal mouse_came_right
 signal mouse_pressed
 signal mouse_released
 signal weapon_changed(index)
+signal reload
 
 onready var viewport := get_viewport()
 onready var is_mouse_on_left := get_mouse_position().x < get_viewport_rect().size.x / 2.0
@@ -36,6 +37,8 @@ func _unhandled_input(event: InputEvent) -> void:
 		emit_signal("weapon_changed", -1)
 	if event.is_action_pressed("-"):
 		emit_signal("weapon_changed", -2)
+	if event.is_action_pressed("reload"):
+		emit_signal("reload")
 	for i in range(1, 10):
 		if (event.is_action_pressed("button_" + str(i))):
 			emit_signal("weapon_changed", i - 1)
