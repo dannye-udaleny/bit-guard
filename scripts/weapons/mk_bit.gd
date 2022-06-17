2794863b5e966b7b2c000a2bcadb0cc01ab37c05
altered <altered_games@outlook.com>
2022-06-17 21:51:04 +0500

    умер от повторений в коде так что починил оружия

--- scripts/weapons/mk_bit.gd
+++ scripts/weapons/mk_bit.gd
@@ -1,38 +1,18 @@
 extends Weapon
 
-export var bullet_speed: float
 
-func mouse_pressed() -> void:
-	if $shoot_cooldown.is_stopped():
-		shoot()
-		$shoot_cooldown.start()
-
-
-func mouse_released() -> void:
-	play("idle")
-
-
-func set_flipped(flipped: bool) -> void:
-	flip_v = flipped
-	offset.y = 3 if flipped else -3
-	$tip.position.y = 1.5 if flipped else -1.5
-
-
-func shoot() -> void:
-	if not Input.is_action_pressed("shoot"):
-		return
-	play("shoot")
+func process_shooting() -> void:
 	var bullet: Projectile = projectile_scene.instance()
 	$"/root".add_child(bullet)
 	bullet.global_position = $tip.global_position
 #	bullet.rotation = get_parent().rotation
 	bullet.direction = bullet.direction.rotated(bullet.rotation)
 	bullet.launch(global_position.direction_to(get_global_mouse_position()), bullet_speed)
-	$shoot_cooldown.start()
-	emit_signal("shot")
+
 
 func start_reload() -> void:
 	play("reload")
+
 
 func end_reload() -> void:
 	play("idle")
--- scripts/weapons/mk_invaider.gd
+++ scripts/weapons/mk_invaider.gd
@@ -1,29 +1,9 @@
 extends Weapon
 
-export var bullet_speed: float
 export var randomness: float
 
 
-func mouse_pressed() -> void:
-	if $shoot_cooldown.is_stopped():
-		shoot()
-		$shoot_cooldown.start()
-
-
-func mouse_released() -> void:
-	play("idle")
-
-
-func set_flipped(flipped: bool) -> void:
-	flip_v = flipped
-	offset.y = 3 if flipped else -3
-	$tip.position.y = 1.5 if flipped else -1.5
-
-
-func shoot() -> void:
-	if not Input.is_action_pressed("shoot"):
-		return
-	play("shoot")
+func process_shooting() -> void:
 	for i in range(-2,3):
 		var angle = i * PI / 24
 		var bullet_1: Projectile = projectile_scene.instance()
@@ -31,11 +11,11 @@ func shoot() -> void:
 		bullet_1.global_position = $tip.global_position
 #		bullet_1.rotation = get_parent().rotation
 		bullet_1.launch((global_position.direction_to(get_global_mouse_position())).rotated(angle), bullet_speed)
-	$shoot_cooldown.start()
-	emit_signal("shot")
+	
 	
 func start_reload() -> void:
 	play("reload")
+	
 	
 func end_reload() -> void:
 	play("idle")
